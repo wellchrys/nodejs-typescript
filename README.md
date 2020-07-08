2396c0692d646bc2d2c0773cf900f045f5a1dd60
wellchrys <chrystiandejesus@hotmail.com>
2020-07-08 12:16:02 -0300

    refactor: drop all files

--- README.md
+++
@@ -1,2 +0,0 @@
-# nodejs-typescript
-Node.js and TypeScript with ts-node-dev, ESLint, Prettier, EditorConfig.
--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import appointments from './appointments.routes';
-
-const routes = Router();
-
-routes.use('/appointments', appointments);
-
-export default routes;
