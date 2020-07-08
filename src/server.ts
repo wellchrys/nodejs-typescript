f22052a304e987fbf73aa701330d0891fd9c6fb4
wellchrys <chrystiandejesus@hotmail.com>
2020-07-08 12:20:18 -0300

    build: add files

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
+});
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333');
+});
